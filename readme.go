bd11408e2f1878a118c239f58001a863294bf973
tso <tso@teknik.io>
2015-12-04 20:12:31 -0500

    improve readme for godoc (hopefully)

--- readme.go
+++ readme.go
@@ -51,49 +51,66 @@
 //
 // Usage:
 //	package main
-//
+//	
 //	import (
-//		"fmt"
-//		"image"
-//      _ "image/jpeg"
-//		"log"
-//		"os"
+//	  "fmt"
+//	  "image"
+//	  _ "image/jpeg"
+//	  "log"
+//	  "os"
+//	  "github.com/generaltso/vibrant"
 //	)
-//
-//	import "github.com/generaltso/vibrant"
-//
-//	func main() {
-//		file, err := os.Open("some_image.jpg")
-//		if err != nil {
-//			log.Fatalln(err)
-//		}
-//		img, _, err := image.Decode(file)
-//		if err != nil {
-//			log.Fatalln(err)
-//		}
-//		palette, err := vibrant.NewPaletteFromImage(img)
-//		if err != nil {
-//			log.Fatalln(err)
-//		}
-//		for name, swatch := range palette.ExtractAwesome() {
-//			fmt.Printf("name: %s, color: %s, population: %d\n", name /* or swatch.Name */, swatch.RGBHex(), swatch.Population)
-//		}
+//	
+//	func checkErr(err error) {
+//	  if err != nil {
+//	      log.Fatalln(err)
+//	  }
 //	}
+//	
+//	func main() {
+//	  file, err := os.Open("some_image.jpg")
+//	  checkErr(err)
+//	  img, _, err := image.Decode(file)
+//	  checkErr(err)
+//	  palette, err := vibrant.NewPaletteFromImage(img)
+//	  checkErr(err)
+//	  for name, swatch := range palette.ExtractAwesome() {
+//	      fmt.Printf("name: %- 12s ", name /* or swatch.Name */)
+//	      fmt.Printf("color: %s ", swatch.RGBHex())
+//	      fmt.Printf("population: %d\n", swatch.Population)
+//	  }
+//	}
+//	
+//	name: DarkMuted    color: #44422f population: 1050
+//	name: Vibrant      color: #f2966c population: 153
+//	name: LightVibrant color: #f4b38a population: 87
+//	name: DarkVibrant  color: #662f2a population: 185
+//	name: Muted        color: #a2555f population: 2722
+//	name: LightMuted   color: #d3aeaf population: 3469
 //
 // There is also a command-line tool in vibrant/:
-//    $ cd vibrant && go install
-//    $ vibrant some_image.png
-// (it prints CSS to stdout)
+//  $ cd vibrant && go install
+//  $ vibrant some_image.png
 //
-// And there is a simple demo application which is a little more visual:
-//    $ cd demo && go run app.go
-//    Listening on 0.0.0.0:8080...
-// (screenshot: http://var.abl.cl/awesome.png)
+//  .lightmuted{background-color:#bec8c8;color:#000000;}
+//  .darkmuted{background-color:#444230;color:#ffffff;}
+//  .vibrant{background-color:#f2966c;color:#000000;}
+//  .lightvibrant{background-color:#e27768;color:#000000;}
+//  .darkvibrant{background-color:#783832;color:#ffffff;}
+//  .muted{background-color:#a2555f;color:#ffffff;}
+//
+// And there is a simple demo web application which is a little more visual:
+//  $ cd demo && go run app.go
+//  Listening on 0.0.0.0:8080...
+//
+//  (screenshot: http://var.abl.cl/awesome.png)
 //
 // This API and the tools provided are a work-in-progress proof-of-concept
 // and certainly could use improvement, better naming, etc.
 //
 // Comments, feedback and pull requests are welcome!
+//
 // tso@teknik.io
+//
 // https://github.com/generaltso/vibrant
 package vibrant
